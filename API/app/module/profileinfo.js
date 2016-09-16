d66de15bcfba88f5dbd128db7d8f7adaf3dd2962
sakthi3964 <sakthi3964@gmail.com>
2016-09-17 00:16:07 +0530

    profile info.js

---
+++ API/app/module/profileinfo.js
@@ -0,0 +1,90 @@
+module.exports = {
+    UserDetial: function (sequelize, Sequelize, modelName) {
+        var User = sequelize.define('profileinfos', {
+            course: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     notEmpty: true
+                // }
+            },
+             profile_id:{
+                  type: Sequelize.STRING,
+             },
+            department: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     notEmpty: true
+                // }
+            },
+            institution: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     notEmpty: true
+                // }
+            },
+            reference: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     notEmpty: true
+                // }
+            },
+
+            commitment: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     notEmpty: true
+                // }
+            },
+
+            designation: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     //notEmpty: true
+                // }
+
+            },
+            organization: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     //notEmpty: true
+                // }
+
+            },
+            area_of_expertise: {
+                type: Sequelize.STRING,
+                // validate: {
+                //    // notEmpty: true
+                // }
+
+            },
+            experience: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     //notEmpty: true
+                // }
+            },
+            cv: {
+                type: Sequelize.STRING,
+                // validate: {
+                //    // notEmpty: true
+                // }
+            },
+            
+            photo: {
+                type: Sequelize.STRING,
+                // validate: {
+                //     //notEmpty: true
+                // }
+            },
+        
+            active: {
+                type: Sequelize.INTEGER,
+                // validate: {
+                //     //notEmpty: true
+                // }
+            }
+
+        });
+        return User;
+    }
+}
