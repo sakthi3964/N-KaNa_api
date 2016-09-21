58b6834cad70e8b1faa7741c47315be2f618cca9
sakthi3964 <sakthi3964@gmail.com>
2016-09-21 21:47:16 +0530

    added child registration

--- API/app/router/router.js
+++ API/app/router/router.js
@@ -2,15 +2,21 @@ module.exports = function (databaseBS, Sequelize) {
     var trackerRouter = require('../controller/trackerController')(databaseBS, Sequelize);
     var childrenReviewRouter = require('../controller/childrenReviewController')(databaseBS, Sequelize);
     var registrationRouter = require('../controller/registrationController')(databaseBS, Sequelize);
+    var profileRouter = require('../controller/profileController')(databaseBS,Sequelize);
+    var childrenProfileRouter = require('../controller/childrenProfileController')(databaseBS,Sequelize);
+    
     var ApiRouter = {};
     ApiRouter.apiRouters = function (router) {
 
-        router.get('/trackerDates', trackerRouter.ListDates);// list dates at the graph module
+        router.post('/trackerDates', trackerRouter.ListDates);// list dates at the graph module
         router.post('/tracker', trackerRouter.Tracker);// tracker form with the holistic indicators
         router.post('/review', childrenReviewRouter.ChildrenReview);// child review form
         router.post('/viewReviewDetail', trackerRouter.ReviewGraph);//graph generation
         router.post('/registration', registrationRouter.Registration);// insert data for registration
-
+        router.post('/validateuser', registrationRouter.ValidateUser);//validate users at login
+        router.get('/viewvolunteer',profileRouter.viewVolunteer);//view the volunteer profile details
+        router.post('/preassess',childrenProfileRouter.Preassess);
+        router.post('/childrenregistration',childrenProfileRouter.childregistration);
     }
     return ApiRouter;
 }
